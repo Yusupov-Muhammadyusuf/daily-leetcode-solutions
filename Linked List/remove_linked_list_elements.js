b14ea2a4a9114b630f51defaff82a959e779d993
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2026-04-18 05:22:05 +0500

    add Remove Linked List Elements problem solution

--- Linked List/remove_linked_list_elements.js
+++ Linked List/remove_linked_list_elements.js
@@ -4,7 +4,7 @@
  * * Description:
  * Given the head of a linked list and an integer val, remove all the nodes 
  * of the linked list that has Node.val == val, and return the new head.
- * * Example:
+ * * Example: 
  * Input: head = [1,2,6,3,4,5,6], val = 6
  * Output: [1,2,3,4,5]
  * * Approach: Dummy Node (Sentinel Node)
